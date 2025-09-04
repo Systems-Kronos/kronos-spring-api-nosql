6bdc029ecd2c61eb23699f0a834626231c6f6311
Yasmin <yasmin.rodrigues@germinare.org.br>
2025-09-03 21:29:20 -0300

    refactor: removendo CRUD's errados

--- src/main/java/com/kronosapinosql/repository/TarefaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.kronosapinosql.repository;
-
-import com.kronosapinosql.model.Tarefa;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface TarefaRepository extends MongoRepository<Tarefa, String>{
-
-}
