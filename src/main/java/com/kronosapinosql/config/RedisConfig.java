56af596002e3db2ed6119e0974805910371c71d6
Yasmin <yasmin.rodrigues@germinare.org.br>
2025-10-08 11:29:35 -0300

    fix: removendo espaços aleatórios, arrumando model e adicionando método de buscar por id gestor

--- src/main/java/com/kronosapinosql/config/RedisConfig.java
+++ src/main/java/com/kronosapinosql/config/RedisConfig.java
@@ -9,9 +9,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
-
     @Bean
-
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
@@ -23,4 +21,3 @@ public class RedisConfig {
         return template;
     }
 }
-
--- src/main/java/com/kronosapinosql/model/Notificacao.java
+++ src/main/java/com/kronosapinosql/model/Notificacao.java
@@ -1,14 +1,11 @@
 package com.kronosapinosql.model;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Field;
 import org.springframework.data.redis.core.RedisHash;
 
 @Getter
